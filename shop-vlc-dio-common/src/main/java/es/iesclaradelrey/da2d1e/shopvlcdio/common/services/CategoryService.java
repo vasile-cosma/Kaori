aaa9fef6082ccf9045b2385a8e2d665aa03b602b
Diego <ortizisla10@gmail.com>
2025-12-10 19:13:22 +0100

    10/12/2025 - "Push de los cambios realizados desde Diego Isla"

--- shop-vlc-dio-common/src/main/java/es/iesclaradelrey/da2d1e/shopvlcdio/common/services/CategoryService.java
+++ shop-vlc-dio-common/src/main/java/es/iesclaradelrey/da2d1e/shopvlcdio/common/services/CategoryService.java
@@ -6,7 +6,7 @@ import es.iesclaradelrey.da2d1e.shopvlcdio.common.entities.Category;
 import java.util.List;
 import java.util.Optional;
 
-public interface CategoryService {
+public interface CategoryService{
     List<Category> findAll();
     Optional<Category> findById(Long id);
     Category save(Category item);
